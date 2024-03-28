b71d8263252ca0338c0975f5104a38bcf190e83f
傲娇小霖霖 <61110248+linlin991229@users.noreply.github.com>
2024-03-28 04:53:31 +0000

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -80,53 +80,34 @@
 
       <div class="zimu">
         <ul id="menu_zm">
-          <li class="zm_1"><a href="javascript:void(0);">A</a></li>
-          <li class="zm_2"><a href="javascript:void(0);">B</a></li>
-          <li class="zm_3"><a href="javascript:void(0);">C</a></li>
-          <li class="zm_4"><a href="javascript:void(0);">D</a></li>
-          <li class="zm_5"><a href="javascript:void(0);">F</a></li>
-          <li class="zm_6"><a href="javascript:void(0);">G</a></li>
-          <li class="zm_7"><a href="javascript:void(0);">H</a></li>
-          <li class="zm_8"><a href="javascript:void(0);">J</a></li>
-          <li class="zm_9"><a href="javascript:void(0);">K</a></li>
-          <li class="zm_10"><a href="javascript:void(0);">L</a></li>
-          <li class="zm_11"><a href="javascript:void(0);">M</a></li>
-          <li class="zm_12"><a href="javascript:void(0);">N</a></li>
-          <li class="zm_13"><a href="javascript:void(0);">P</a></li>
-          <li class="zm_14"><a href="javascript:void(0);">Q</a></li>
-          <li class="zm_15"><a href="javascript:void(0);">R</a></li>
-          <li class="zm_16"><a href="javascript:void(0);">S</a></li>
-          <li class="zm_17"><a href="javascript:void(0);">T</a></li>
-          <li class="zm_18"><a href="javascript:void(0);">W</a></li>
-          <li class="zm_19"><a href="javascript:void(0);">X</a></li>
-          <li class="zm_20"><a href="javascript:void(0);">Y</a></li>
-          <li class="zm_21"><a href="javascript:void(0);">Z</a></li>
+            <li v-for="n in 21" :key="n" :class="`zm_${n}`"><a href="javascript:void(0);">{{ String.fromCharCode(64+n) }}</a></li>
         </ul>
       </div>
 
       <div class="xibie">
         <ul id="menu_xi">
-          <li class="xi_1"><a href="javascript:void(0);" rel="" class="">全部</a></li>
-          <li class="xi_2"><a href="javascript:void(0);" rel="3081">火系</a></li>
-          <li class="xi_3"><a href="javascript:void(0);" rel="3078">金系</a></li>
-          <li class="xi_4"><a href="javascript:void(0);" rel="3079">木系</a></li>
-          <li class="xi_5"><a href="javascript:void(0);" rel="3080">水系</a></li>
-          <li class="xi_6"><a href="javascript:void(0);" rel="3082">土系</a></li>
-          <li class="xi_7"><a href="javascript:void(0);" rel="3086">翼系</a></li>
-          <li class="xi_8"><a href="javascript:void(0);" rel="3087">怪系</a></li>
-          <li class="xi_9"><a href="javascript:void(0);" rel="3084">魔系</a></li>
-          <li class="xi_10"><a href="javascript:void(0);" rel="3083">妖系</a></li>
-          <li class="xi_11"><a href="javascript:void(0);" rel="3908">风系</a></li>
-          <li class="xi_12"><a href="javascript:void(0);" rel="4314">毒系</a></li>
-          <li class="xi_13"><a href="javascript:void(0);" rel="4565">雷系</a></li>
-          <li class="xi_14"><a href="javascript:void(0);" rel="4738">幻系</a></li>
-          <li class="xi_15"><a href="javascript:void(0);" rel="5364">冰系</a></li>
-          <li class="xi_16"><a href="javascript:void(0);" rel="5742">灵系</a></li>
-          <li class="xi_17"><a href="javascript:void(0);" rel="6070">机械</a></li>
-          <li class="xi_18"><a href="javascript:void(0);" rel="6426">火风系</a></li>
-          <li class="xi_19"><a href="javascript:void(0);" rel="7556">木灵系</a></li>
-          <li class="xi_sx"><a href="javascript:void(0);" rel="15744">圣系</a></li>
-          <li class="xi_20"><a href="javascript:void(0);" rel="30272">土幻系</a></li>
+          <li v-for="n of 21" :class="`xi_${n}`" :key="n"><a href="javascript:void(0);">全部</a></li>
+          <!-- <li class="xi_1"><a href="javascript:void(0);">全部</a></li>
+          <li class="xi_2"><a href="javascript:void(0);">火系</a></li>
+          <li class="xi_3"><a href="javascript:void(0);">金系</a></li>
+          <li class="xi_4"><a href="javascript:void(0);">木系</a></li>
+          <li class="xi_5"><a href="javascript:void(0);">水系</a></li>
+          <li class="xi_6"><a href="javascript:void(0);">土系</a></li>
+          <li class="xi_7"><a href="javascript:void(0);">翼系</a></li>
+          <li class="xi_8"><a href="javascript:void(0);">怪系</a></li>
+          <li class="xi_9"><a href="javascript:void(0);">魔系</a></li>
+          <li class="xi_10"><a href="javascript:void(0);">妖系</a></li>
+          <li class="xi_11"><a href="javascript:void(0);">风系</a></li>
+          <li class="xi_12"><a href="javascript:void(0);">毒系</a></li>
+          <li class="xi_13"><a href="javascript:void(0);">雷系</a></li>
+          <li class="xi_14"><a href="javascript:void(0);">幻系</a></li>
+          <li class="xi_15"><a href="javascript:void(0);">冰系</a></li>
+          <li class="xi_16"><a href="javascript:void(0);">灵系</a></li>
+          <li class="xi_17"><a href="javascript:void(0);">机械</a></li>
+          <li class="xi_18"><a href="javascript:void(0);">火风系</a></li>
+          <li class="xi_19"><a href="javascript:void(0);">木灵系</a></li>
+          <li class="xi_sx"><a href="javascript:void(0);" >圣系</a></li>
+          <li class="xi_20"><a href="javascript:void(0);" >土幻系</a></li> -->
         </ul>
       </div>
     </div>
