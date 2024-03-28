3eb16120f400fa53758001ace20fbfa777104a6d
xianlin <xianlin@intasect.com.cn>
2024-03-28 09:16:22 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="bg">
+    <div class="head_1">
+      <div class="to-game">
+        <NuxtLink class="link-to-game">爱卡布，爱西游，冲啊</NuxtLink>
+      </div>
+    </div>
+    <div class="head_2" />
+    <div class="nav-bg">
+      <div class="logo">
+        <NuxtLink class="link-to-game">爱卡布，爱西游，冲啊</NuxtLink>
+      </div>
+      <div class="menu">
+        <ul>
+          <li class="nav_1">
+            <a href="//www.4399.com/flash/48399.htm">4399卡布西游</a>
+          </li>
+          <li class="nav_2">
+            <a href="//news.4399.com/kabuxiyou/yaoguaidaquan/"
+              >卡布西游妖怪大全</a
+            >
+          </li>
+          <li class="nav_3">
+            <a href="//v.4399pk.com/kabuxiyou/">卡布西游视频</a>
+          </li>
+          <li class="nav_4">
+            <a href="//news.4399.com/kabuxiyou/gonglve/">卡布西游攻略</a>
+          </li>
+          <li class="nav_5">
+            <a href="//news.4399.com/kabuxiyou/jingyan/">卡布西游心得</a>
+          </li>
+          <li class="nav_6">
+            <a href="//news.4399.com/kabuxiyou/zixun/">卡布西游最新资讯</a>
+          </li>
+          <li class="nav_7">
+            <a href="//news.4399.com/kabuxiyou/xszn/">卡布西游新手指南</a>
+          </li>
+          <li class="nav_8 no_mar">
+            <a href="//my.4399.com/forums-mtag-tagid-79936.html"
+              >卡布西游论坛</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="yg_t">
+      <div class="yg-top">
+        <div class="title_yg" />
+        <div class="cl_3">
+          <ul class="menu_att">
+            <li class="cl_yg1">
+              <a id="allcw" href="" class="on" rel="">全部妖怪</a>
+            </li>
+            <li class="cl_yg2">
+              <a id="dawang" href="" rel="15">妖怪大全</a>
+            </li>
+            <li class="cl_yg3">
+              <a id="xiyoucw" href="" rel="14">稀有妖怪</a>
+            </li>
+            <li class="cl_yg6">
+              <a id="chaojinhua" href="" rel="36">超进化</a>
+            </li>
+            <li class="cl_yg7">
+              <a href="" id="chaojinhua" rel="3738">珍宠神宠</a>
+            </li>
+            <li class="cl_yg4">
+              <a id="vipyaoguai" href="" rel="20">VIP妖怪</a>
+            </li>
+            <li class="cl_yg5">
+              <a
+                href="//news.4399.com/kabuxiyou/NPCdaquan/"
+                title="NPC大全"
+                rel="NPC"
+                >NPC大全</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="zimu">
+        <ul id="menu_zm">
+          <li class="zm_1"><a href="javascript:void(0);">A</a></li>
+          <li class="zm_2"><a href="javascript:void(0);">B</a></li>
+          <li class="zm_3"><a href="javascript:void(0);">C</a></li>
+          <li class="zm_4"><a href="javascript:void(0);">D</a></li>
+          <li class="zm_5"><a href="javascript:void(0);">F</a></li>
+          <li class="zm_6"><a href="javascript:void(0);">G</a></li>
+          <li class="zm_7"><a href="javascript:void(0);">H</a></li>
+          <li class="zm_8"><a href="javascript:void(0);">J</a></li>
+          <li class="zm_9"><a href="javascript:void(0);">K</a></li>
+          <li class="zm_10"><a href="javascript:void(0);">L</a></li>
+          <li class="zm_11"><a href="javascript:void(0);">M</a></li>
+          <li class="zm_12"><a href="javascript:void(0);">N</a></li>
+          <li class="zm_13"><a href="javascript:void(0);">P</a></li>
+          <li class="zm_14"><a href="javascript:void(0);">Q</a></li>
+          <li class="zm_15"><a href="javascript:void(0);">R</a></li>
+          <li class="zm_16"><a href="javascript:void(0);">S</a></li>
+          <li class="zm_17"><a href="javascript:void(0);">T</a></li>
+          <li class="zm_18"><a href="javascript:void(0);">W</a></li>
+          <li class="zm_19"><a href="javascript:void(0);">X</a></li>
+          <li class="zm_20"><a href="javascript:void(0);">Y</a></li>
+          <li class="zm_21"><a href="javascript:void(0);">Z</a></li>
+        </ul>
+      </div>
+
+      <div class="xibie">
+        <ul id="menu_xi">
+          <li class="xi_1"><a href="javascript:void(0);" rel="" class="">全部</a></li>
+          <li class="xi_2"><a href="javascript:void(0);" rel="3081">火系</a></li>
+          <li class="xi_3"><a href="javascript:void(0);" rel="3078">金系</a></li>
+          <li class="xi_4"><a href="javascript:void(0);" rel="3079">木系</a></li>
+          <li class="xi_5"><a href="javascript:void(0);" rel="3080">水系</a></li>
+          <li class="xi_6"><a href="javascript:void(0);" rel="3082">土系</a></li>
+          <li class="xi_7"><a href="javascript:void(0);" rel="3086">翼系</a></li>
+          <li class="xi_8"><a href="javascript:void(0);" rel="3087">怪系</a></li>
+          <li class="xi_9"><a href="javascript:void(0);" rel="3084">魔系</a></li>
+          <li class="xi_10"><a href="javascript:void(0);" rel="3083">妖系</a></li>
+          <li class="xi_11"><a href="javascript:void(0);" rel="3908">风系</a></li>
+          <li class="xi_12"><a href="javascript:void(0);" rel="4314">毒系</a></li>
+          <li class="xi_13"><a href="javascript:void(0);" rel="4565">雷系</a></li>
+          <li class="xi_14"><a href="javascript:void(0);" rel="4738">幻系</a></li>
+          <li class="xi_15"><a href="javascript:void(0);" rel="5364">冰系</a></li>
+          <li class="xi_16"><a href="javascript:void(0);" rel="5742">灵系</a></li>
+          <li class="xi_17"><a href="javascript:void(0);" rel="6070">机械</a></li>
+          <li class="xi_18"><a href="javascript:void(0);" rel="6426">火风系</a></li>
+          <li class="xi_19"><a href="javascript:void(0);" rel="7556">木灵系</a></li>
+          <li class="xi_sx"><a href="javascript:void(0);" rel="15744">圣系</a></li>
+          <li class="xi_20"><a href="javascript:void(0);" rel="30272">土幻系</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- 内容 -->
+    <slot/>
+    <!-- 结尾 -->
+    <div class="yg_c">
+      <div class="more_yg" id="showmorejingling">
+        <a href=""><img src="../assets/images/more_yg.gif" /></a>
+      </div>
+    </div>
+    <div class="yg_b" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import "../assets/css/yaoguaodaquan.css";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="yg-list">
+    <span>妖怪列表</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.yg-list {
+  background: url('../assets/images/c_6.gif') repeat-y;
+  width: 990px;
+  margin: 0 auto;
+}
+
+.yg-list ul {
+  display: block;
+}
+</style>
